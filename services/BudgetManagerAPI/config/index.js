fec35e8551a40c6b763470fe3fa918ae04636f47
Luiza <luitays@mail.ru>
2019-06-10 23:44:50 +0300

    init backend with jwt authentication from tutorial

---
+++ services/BudgetManagerAPI/config/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  secret: 'budgetsecret',
+  session: { session: false },
+  database: 'mongodb+srv://admin:admin2019@cluster0-rw9jp.mongodb.net/test?retryWrites=true&w=majority'
+}
