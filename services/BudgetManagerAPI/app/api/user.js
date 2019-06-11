43d815b2ec89dfaf1e3ddd0b95391b8ffbf6e5a3
Luiza <luitays@mail.ru>
2019-06-11 12:20:39 +0300

    removed test methods

--- services/BudgetManagerAPI/app/api/user.js
+++ services/BudgetManagerAPI/app/api/user.js
@@ -2,38 +2,12 @@ const mongoose = require('mongoose');
 
 const api = {};
 
-api.setup = (User) => (req, res) => {
-  const admin = new User({
-    username: 'admin',
-    password: 'admin',
-    clients: []
-  });
-
-admin.save(error => {
-    if (error) throw error;
-
-console.log('Admin account was succesfully set up');
-    res.json({ success: true });
-  })
-}
-
-api.index = (User, BudgetToken) => (req, res) => {
-    const token = BudgetToken;
-if (token) {
-    User.find({}, (error, users) => {
-      if (error) throw error;
-      res.status(200).json(users);
-    });
-  } else return res.status(403).send({ success: false, message: 'Unauthorized' });
-}
-
 api.signup = (User) => (req, res) => {
   if (!req.body.username || !req.body.password) res.json({ success: false, message: 'Please, pass a username and password.' });
   else {
     const newUser = new User({
       username: req.body.username,
-      password: req.body.password,
-      clients: []
+      password: req.body.password
     });
 
     newUser.save((error) => {
@@ -44,3 +18,4 @@ api.signup = (User) => (req, res) => {
 }
 
 module.exports = api;
+
